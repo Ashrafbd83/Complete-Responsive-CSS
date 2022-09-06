48369bb03ef04bd20249636dcc0296998a1dec9b
Ashraf <ashrafbd83@gmail.com>
2022-09-06 14:20:48 +0600

    Remove old file and add myproject file

---
+++ main.js
@@ -0,0 +1,15 @@
+// Execut script ofter page load
+
+window.onload=function digital_fn(){
+    //toggle button
+let toggle = document.querySelector(".toggle-btn");
+let collapse = document.querySelector(".collapse");
+
+toggle.addEventListener("click",function(event){
+    collapse.classList.toggle("active");
+});
+  
+
+
+    // console.log(toggle);
+};  collapse.classList.toggle(active);
